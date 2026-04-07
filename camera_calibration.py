295c7ebf4b67760e3c0ef93505452d741e9b45bc
jun <sjune21@daum.net>
2026-04-07 23:08:12 +0900

    최종

--- camera_calibration.py
+++ camera_calibration.py
@@ -3,6 +3,8 @@ import cv2 as cv
 import datetime
 
 TW = 800 # Target width for display
+#is_paused = False
+#is_selecting = False
 
 
 def select_img_from_video(video_file, board_pattern, recorder, w, h, select_all=False, wait_msec=10, wnd_name='Camera Calibration'):
@@ -12,13 +14,14 @@ def select_img_from_video(video_file, board_pattern, recorder, w, h, select_all=
 
     # Select images
     img_select = []
-    while True:
-        # Grab an images from the video
-        valid, img = video.read()
+    # Grab an images from the video
+    valid, img = video.read()
 
-        if not valid:
-            break
+    is_paused = False
+    is_selecting = False
 
+    while valid:
+        # 프레임 크기 조절
         dim = (w, h)
         img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
 
@@ -28,26 +31,32 @@ def select_img_from_video(video_file, board_pattern, recorder, w, h, select_all=
             # Show the image
             display = img.copy()
             cv.putText(display, f'NSelect: {len(img_select)}', (10, 25), cv.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0))
+
+            if is_paused:             # Space: Pause and show corners
+                complete, pts = cv.findChessboardCorners(img, board_pattern)
+                cv.drawChessboardCorners(display, board_pattern, pts, complete)
+            elif is_selecting:        # Enter: Select the image
+                img_select.append(img)
+                is_selecting = False
+
             cv.imshow(wnd_name, display)
             recorder.write(display) # Record the video with the display
 
-            # Process the key event
-            key = cv.waitKey(wait_msec)
-            if key == ord(' '):             # Space: Pause and show corners
-                complete, pts = cv.findChessboardCorners(img, board_pattern)
-                cv.drawChessboardCorners(display, board_pattern, pts, complete)
-                cv.imshow(wnd_name, display)
-                recorder.write(display) # Record the video with the display
-                key = cv.waitKey()
-                while key != ord('\r'): # Space: Resume
-                    recorder.write(display) # Record the video with the display
-                    key = cv.waitKey()
+        # 키 입력 처리
+        key = cv.waitKey(wait_msec)
+          
+        if key == ord(' '):     # Space: Pause and show corners
+            is_paused = not is_paused
+        elif key == ord('\r'):  # Enter: Select the image
+            if is_paused:
+                is_selecting = not is_selecting
+                is_paused = False # 선택 후 일시정지 해제
+        elif key == 27:     # ESC: Exit (Complete image selection)
+            break
 
-                img_select.append(img) # Enter: Select the image
-
-                    
-            if key == 27:                  # ESC: Exit (Complete image selection)
-                break
+        # 일시정지 상태가 아닐 때만 다음 프레임을 읽음
+        if not is_paused:
+            valid, img = video.read()
 
     cv.destroyAllWindows()
     return img_select
@@ -69,7 +78,7 @@ def calib_camera_from_chessboard(images, board_pattern, board_cellsize, K=None, 
     # Calibrate the camera
     return cv.calibrateCamera(obj_points, img_points, gray.shape[::-1], K, dist_coeff, flags=calib_flags)
 
-def distortion_correction(target_video, target_K, target_dist_coeff, recorder, w, h):
+def distortion_correction(target_video, target_K, target_dist_coeff, recorder, result_recorder, w, h):
     # The given video and calibration data
     video_file = target_video
     K = target_K # Derived from `calibrate_camera.py`
@@ -82,6 +91,25 @@ def distortion_correction(target_video, target_K, target_dist_coeff, recorder, w
     # Run distortion correction
     show_rectify = True
     map1, map2 = None, None
+    # 결과 영상 저장
+    while True:
+        valid, img = video.read()
+        if not valid: 
+            video = cv.VideoCapture(video_file)
+            map1, map2 = None, None
+            break
+        
+        img = cv.resize(img, (w, h), interpolation=cv.INTER_AREA)
+        
+        if map1 is None or map2 is None:
+            map1, map2 = cv.initUndistortRectifyMap(target_K, target_dist_coeff, None, None, (w, h), cv.CV_32FC1)
+        
+        # 보정 수행
+        rectified_img = cv.remap(img, map1, map2, interpolation=cv.INTER_LINEAR)
+        
+        # 파일에 결과 저장
+        result_recorder.write(rectified_img)
+    # 재생 및 비교
     while True:
         # Read an image from the video
         valid, img = video.read()
@@ -133,7 +161,7 @@ if __name__ == '__main__':
     out_recorder = cv.VideoWriter("Play_" + video_name + "_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".avi", fourcc, fps, (target_width, target_height))
     #결과 영상
     result_recorder = cv.VideoWriter("Result_" + video_name + "_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".avi", fourcc, fps, (target_width, target_height))
-
+    result_play_recorder = cv.VideoWriter("Result_Play_" + video_name + "_" + datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".avi", fourcc, fps, (target_width, target_height))
     img_select = select_img_from_video(video_file, board_pattern, out_recorder, target_width, target_height)
     assert len(img_select) > 0, 'There is no selected images!'
     rms, K, dist_coeff, rvecs, tvecs = calib_camera_from_chessboard(img_select, board_pattern, board_cellsize)
@@ -146,7 +174,7 @@ if __name__ == '__main__':
     print(f'* Distortion coefficient (k1, k2, p1, p2, k3, ...) = {dist_coeff.flatten()}')
 
     # Run distortion correction
-    distortion_correction(video_file, K, dist_coeff, result_recorder, target_width, target_height)
+    distortion_correction(video_file, K, dist_coeff, result_play_recorder, result_recorder, target_width, target_height)
 
     # 모든 작업 완료 후
     out_recorder.release()
